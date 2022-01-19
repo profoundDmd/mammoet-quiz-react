16f7d9adb9a24528cdea5abfccd36236fe64f072
jninkovi <jadrandub@gmail.com>
2022-01-19 15:45:15 +0100

    Added sounds & switch-statement

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,19 +1,39 @@
 import React from 'react';
 import './Button.scss'
+import stoneClick from './../../assets/sounds/stone-button-click.mp3'
+import woodClick from './../../assets/sounds/wood-button-click.mp3'
 
 const Button = ({text, type, clickEvent}) => {
     const buttonType = () => {
-        if(type === "stone"){
-            return "stone";
-        }else if(type === "wood"){
-            return "wood";
-        }else{
-            return "";
+        switch (type){
+            case "stone":
+                return "stone";
+            case "wood" :
+                return "wood";
+            default:
+                console.log("no buttonType for this");
+                return "";
         }
     };
 
+    const playSound = () => {
+        switch (type){
+            case "stone":
+                new Audio (stoneClick).play();
+                break;
+            case "wood":
+                new Audio (woodClick).play();
+                break;
+            default :
+                console.log("no audio for this");
+        }
+    }
+
     return (
-        <button className={"mainButton " + buttonType()} onClick={clickEvent}>
+        <button className={"mainButton " + buttonType()} onClick={ () => {
+            playSound();
+            clickEvent();
+        }}>
             {text}
         </button>
     );
