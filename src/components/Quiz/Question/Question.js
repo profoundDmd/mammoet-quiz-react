e29664d3119b23f4ba3a0b469fa2a2f346ae93c7
jninkovi <jadrandub@gmail.com>
2022-02-23 16:13:43 +0100

    questions

--- src/components/Quiz/Question/Question.js
+++ src/components/Quiz/Question/Question.js
@@ -1,33 +1,58 @@
 import React, {useState} from 'react';
 import "./Question.scss";
+import YouTube from "react-youtube";
+import {AnimatePresence} from "framer-motion";
 
 const Question = ({question, setCurrentQuestion}) => {
 
-    const click = () => {
+    const [askQuestion, setAskQuestion] = useState(false);
+
+    const opts = {height: '390', width: '640', playerVars: {autoplay: 1,}, origin: 'http://localhost:3000',};
+
+    const onYtIntroReady = () => {
+
+    }
+
+    const onYtIntroEnd = () => {
+
+    }
+
+    const onYtIntroExitComplete = () => {
+
+    }
+
+    const nextQuestion = () => {
         setCurrentQuestion(cq => {
-            console.log(cq);
             return ++cq;
         });
     }
 
     return (
-        <>
-            <div className="question">
-                {question.question}
-                {
-                    question.answers.map(
-                        answer => { return(
-                            <div key={`answer_${answer.id}`}>
-                                <div>${answer.answer}</div>
-                                <div>${answer.isCorrect}</div>
-                            </div>
-                        )}
-                    )
-                }
+        <div className="question">
+            <AnimatePresence onExitComplete={onYtIntroExitComplete}>
+                <div className="modalBg" />
+                <YouTube
+                    videoId="452kpneADrA"
+                    opts={opts}
+                    onReady={onYtIntroReady}
+                    onEnd={onYtIntroEnd}
+                    className="youtubeVid"/>
+            </AnimatePresence>
 
-                <button onClick={click}> klik</button>
-            </div>
-        </>
+            {question.question}
+            {
+                question.answers.map(
+                    answer => { return(
+                        <div key={`answer_${answer.id}`}>
+                            <div>${answer.answer}</div>
+                            <div>${answer.isCorrect}</div>
+                        </div>
+                    )}
+                )
+            }
+
+            <button onClick={nextQuestion}> klik</button>
+        </div>
     );
 };
 
