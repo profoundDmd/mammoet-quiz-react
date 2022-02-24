99f99b99944fae0f12096fb9b70462b75d777ca8
jninkovi <jadrandub@gmail.com>
2022-02-24 16:48:38 +0100

    answers

--- src/pages/Quiz/Answer/Answer.js
+++ src/pages/Quiz/Answer/Answer.js
@@ -1,9 +1,25 @@
 import React from 'react';
 
 const Answer = ({id, answer, isCorrect}) => {
+    const onClick = () => {
+        if(isCorrect){}
+        else{}
+    }
+
+    const back = () => {
+        if(isCorrect){return <div>Joepi, het goede antwoord!</div>}
+        return <div>Helaas, fout antwoord</div>
+    }
+
     return (
-        <div key={`answer_${id}`} className="answer">
-            <div>${answer}</div>
+        <div key={`answer_${id}`} className="answer" onClick={onClick}>
+            <div className="front">
+                <div>${answer}</div>
+            </div>
+            <div className="back">
+                {back}
+            </div>
+
             <div>${isCorrect}</div>
         </div>
     );
